c186d0506261f9562ea634c34a4985d50022ab9f
ZJR <mr.it@ZJR.local>
2026-06-02 15:26:06 +0700

    fix: prisma initialization and vercel deployment

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,7 +1,15 @@
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: ["error", "warn"],
+});
 
+// Ensure graceful shutdown
+process.on("SIGINT", async () => {
+  await prisma.$disconnect();
+  process.exit(0);
+});
 
 export default prisma;
 
